a54f3adf2f8d8866cb100bdbf8f8d8185f18bb5c
EugeneSemivolos <semivolos.evgen1@gmail.com>
2023-02-28 21:36:10 +0200

    fix: formatted

--- handler.go
+++ handler.go
@@ -7,24 +7,24 @@ import (
 )
 
 type TimeResponse struct {
-    Time string `json:"time"`
+	Time string `json:"time"`
 }
 
 func GetTime(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
 
-    currentTime := time.Now().Format(time.RFC3339)
+	currentTime := time.Now().Format(time.RFC3339)
 
-    response := TimeResponse{
-        Time: currentTime,
-    }
+	response := TimeResponse{
+		Time: currentTime,
+	}
 
-    jsonResponse, err := json.Marshal(response)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    w.Write(jsonResponse)
+	w.Write(jsonResponse)
 }
