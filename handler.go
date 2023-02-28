24c4f40bb68fe4cbf8a3f4dd7c0d7b0ad6ca0ddd
chyrkovmaksym <chyrkovmaksym1@gmail.com>
2023-02-28 21:22:34 +0200

    feat: created /time request handler

---
+++ handler.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type TimeResponse struct {
+    Time string `json:"time"`
+}
+
+func GetTime(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Header().Set("Content-Type", "application/json")
+
+    currentTime := time.Now().Format(time.RFC3339)
+
+    response := TimeResponse{
+        Time: currentTime,
+    }
+
+    jsonResponse, err := json.Marshal(response)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Write(jsonResponse)
+}
--- server.go
+++ server.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/time", getTime)
+	http.HandleFunc("/time", GetTime)
 	err := http.ListenAndServe(":8795", nil)
 	handleError(err)
 }
